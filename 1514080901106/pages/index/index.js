8ddb9590d7c66a5775ef99425a7374feed67497e
MercyMo <walker.dhr@outlook.com>
2017-11-03 20:44:47 +0800

    replace

--- 1514080901106/pages/index/index.js
+++ 1514080901106/pages/index/index.js
@@ -1,40 +1,27 @@
-var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
-const app = getApp();
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
   data: {
-    tabs: ["滤镜", "调整"],
-    activeIndex: 0,
-    sliderOffset: 0,
-    sliderLeft: 0,
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
-  bindViewTap: function () {
+  //事件处理函数
+  bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-
   onLoad: function () {
-    var that = this;
-    wx.getSystemInfo({
-      success: function (res) {
-        that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
-        });
-      }
-    });
-
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse) {
+    } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -51,21 +38,12 @@ Page({
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
-          });
+          })
         }
-      });
+      })
     }
   },
-
-
-  tabClick: function (e) {
-    this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id
-    });
-  },
-
-  getUserInfo: function (e) {
+  getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -73,4 +51,4 @@ Page({
       hasUserInfo: true
     })
   }
-});
+})
