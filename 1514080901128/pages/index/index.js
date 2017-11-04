23ccf364486bbce5246f4a81cf18a20eda07cd06
FFFfire666 <1141560297@qq.com>
2017-11-05 02:26:11 +0800

    #1 #82 第二次实验代码

--- 1514080901128/pages/index/index.js
+++ 1514080901128/pages/index/index.js
@@ -1,47 +1,59 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+//navbar
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 Page({
   data: {
-    motto: '哦嗨哟，死肥宅',
+    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    //searchbar
+    inputShowed: false,
+    inputVal: "",
+    //navbar
+    tabs: ["推荐", "动画情报", "漫画情报"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0,
+
+    inputShowed: false,
+    inputVal: "",
+    //navbar
+    tabs: ["推荐", "动画情报", "漫画情报"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    //swiper
+    imgUrls: [
+      '../images/01.jpg',
+      '../images/02.jpg',
+      '../images/03.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    circular:true
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
   },
+  //navbar
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -50,5 +62,41 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+  //searchbar
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+  //navbar
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   }
+  //swiper
+  
 })
+
+
+
+
