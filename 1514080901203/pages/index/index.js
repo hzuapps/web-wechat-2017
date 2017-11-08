11a1bbb411fc3093420fcadc06a0a685bd3cf596
cs2JeX <781754546@qq.com>
2017-11-08 20:17:57 +0800

    #1 #92 第2次实验代码

--- 1514080901203/pages/index/index.js
+++ 1514080901203/pages/index/index.js
@@ -4,10 +4,8 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    kdValues: ['顺丰速运','邮政ems','天天快递','圆通快递','申通快递','中通快递','京东快递','其他'],
+    kdValueIndex:3,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,40 +13,19 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+  bindkdValueChange: function (e) {
+
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      kdValueIndex: e.detail.value
+    })
+  },
+  showInfo: function(){
+    wx.navigateTo({
+      url: '../kdInfo/kdInfo',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
     })
   }
 })
