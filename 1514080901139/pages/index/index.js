10c24514a4cdae86e1b2c008f4a7bb5b08dc308b
zhongting <1102365435@qq.com>
2017-11-09 09:25:00 +0800

    #3 #15 第三次实验代码

--- 1514080901139/pages/index/index.js
+++ 1514080901139/pages/index/index.js
@@ -4,9 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    todoList: [{text: '打扫卫生', status: false}, {text: '打扫卫生', status: false}, {text: '打扫卫生', status: false}, {text: '打扫卫生', status: false}], // 待办数组 status表示是否完成 ，未完成为false，完成为true
+    todoList: [{ text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }, { text: '打扫卫生', status: false }], // 待办数组 status表示是否完成 ，未完成为false，完成为true
+
+
     footerInputHidden: true, // 是否隐藏底部input
-    inputValue: '' // TODO input输入之后会修改input的value属性，暂时先每次将它初始化为空，待修改
+    inputValue: '',  // 初始化input
+    isLower: false, // 页面是否到达底部
+    isEnd: false // 是否还有更多数据
   },
   //事件处理函数
   // 添加待办按钮点击
@@ -24,7 +28,7 @@ Page({
   onFooterInputConfirm: function (e) {
     let todoList = this.data.todoList
     let text = e.detail.value
-    let param = {text: text, status: false} 
+    let param = { text: text, status: false }
 
     if (param.text) {  // 如果有输入
       todoList.push(param)
@@ -34,12 +38,12 @@ Page({
         footerInputHidden: true,
         inputValue: ''
       })
-      
+
     }
   },
 
   // 完成圆圈点击
-  onCircleTap: function (e){
+  onCircleTap: function (e) {
     console.log(e)
     let index = e.currentTarget.dataset.index
 
@@ -48,5 +52,42 @@ Page({
     param[string] = !this.data.todoList[index].status
 
     this.setData(param)
+  },
+
+  // 页面滚动到最底部
+  onscrollToLower: function () {
+    console.log('hahaha')
+    let that = this
+
+    if (!this.data.isEnd) {
+      this.setData({
+        isLower: true
+      })
+
+      console.log(this.data.isLower)
+
+      let todoList = this.data.todoList
+
+      if (todoList.length < 20) { // 如果数组长度小于20则加载，用来模拟数据加载完毕的情况
+        todoList.push({ text: '++++', status: false }, { text: '++++', status: false }, { text: '++++', status: false }, { text: '++++', status: false })
+
+        setTimeout(function () {
+          that.setData({
+            isLower: false,
+            todoList: todoList
+          })
+        }, 1500)
+
+      } else {
+        setTimeout(function () {
+          that.setData({
+            isLower: false,
+            isEnd: true
+          })
+        }, 1700)
+      }
+    }
+
+
   }
 })
