555db9bbaa96283d0052c660a65b802e2b47dd0a
fengdejiyi712 <1533067481@qq.com>
2017-11-11 14:45:58 +0800

    #3 #36 第3次实验代码

--- 1514080901222/pages/index/index.js
+++ 1514080901222/pages/index/index.js
@@ -47,5 +47,81 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  onLoad: function (options) {
+    var that = this
+    // 设置窗口大小
+    wx.getSystemInfo({
+      success: (res) => {
+        that.setData({
+          windowHeight: res.windowHeight - 5,
+          windowWidth: res.windowWidth
+        })
+        //console.dir(that.data.windowHeight)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onUpper: function () { },
+  onLower: function () {
+    var that = this
+    that.setData({
+      isLower: true
+    });
+    // load more data
+    console.log("onLower")
+  },
+  onScroll: function () {
+    console.log("onScroll")
   }
+
 })
