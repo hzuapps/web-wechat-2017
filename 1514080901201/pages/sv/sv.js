d0e08ab1ecab93a97b412d29b2b523f7ac131154
CCGyyn <1107079145@qq.com>
2017-11-11 21:51:19 +0800

    #3 #79 第3次实验代码

---
+++ 1514080901201/pages/sv/sv.js
@@ -0,0 +1,236 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    // 设置窗口大小
+    wx.getSystemInfo({
+      success: (res) => {
+        that.setData({
+          windowHeight: res.windowHeight - 5,
+          windowWidth: res.windowWidth
+        })
+        //console.dir(that.data.windowHeight)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onUpper: function () { },
+  onLower: function () {
+    var that = this
+    that.setData({
+      isLower: true
+    });
+    // load more data
+    console.log("onLower")
+  },
+  onScroll: function () {
+    console.log("onScroll")
+  }
+})
+
+// Page({
+//    /**
+//     * 页面的初始数据
+//     */
+//     data: {
+//      list:[
+//           {
+//          CNtitle:"《监狱学园》音乐剧化！外传漫画与本篇第27卷发售",
+//             CNdesc:"平本彰创作的大尺度漫画《监狱学园》宣布了音乐剧化的消息。这次的音乐剧化将在2018年1月31日至2月4日在东京上演。另外在11月6日，漫画的最新卷第27卷和由漫画组合ReDrop创作的外传《副会长加油》也已经发售。",
+//             CNpicture:""
+//           }, {
+//          CNtitle: "果然是换个名字而已！漫画《干物妹！小埋G》将开",
+//             CNdesc: "在前不久曾为大家介绍过，漫画《干物妹！小埋》将于11月9日发售的杂志《Young Jump 50号》上完结，并进行另一个重大发表的消息。目前这个重大发表的内容已经偷跑。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E5%B0%8F%E5%9F%8B.jpg?raw=true"
+//           }, {
+//          CNtitle: "把对手打成草稿？漫画《新网球王子》刊登草稿画面",
+//             CNdesc: "在《Jump Square 12月号》上，有读者发现了漫画《新网球王子》中出现了部分草稿的内容。原作者许斐刚表示，在最新话的内容中，由于遇到了种种麻烦，被迫以未完成原稿的形式刊登。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E7%BD%91%E7%8E%8B.jpg?raw=true"
+//           }, {
+//          CNtitle: "基本上是动画化决定 漫画《汤摇庄的幽奈同学》将有",
+//             CNdesc: "一度曾因为尺度过大，被热衷批评二次元的某律师指责应该进行规制，最后还有报纸发文讨论露出度问题的漫画《汤摇庄的幽奈同学》，宣布将在中间彩页有重大发表。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E5%B9%BD%E5%A5%88.jpg?raw=true"
+//           }, {
+//          CNtitle: "【剧透注意】太坑人！漫画《网球优等生》完结方式遭吐槽",
+//             CNdesc: "在11月1日发售的《周刊少年Magazine 48号》（讲谈社）上，由胜木光创作的漫画《网球优等生》正式完结。本作自2007年开始连载，已经连载了10年。作品的最终47卷，预计将于12月15日发售。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E7%BD%91%E7%90%83%E4%BC%98%E7%AD%89%E7%94%9F.jpg?raw=true"
+//           }, {
+//          CNtitle: "工口本化身大姐姐！大见武士《休刊小姐》单行本发售",
+//             CNdesc: "在日本的神话中，有着万物皆寄宿着神灵的说法。这要是换到二次元的世界中，或许该说成万物皆寄宿着美少女。近日，大见武士创作《休刊小姐》的单行本发售了。在本作中，讲述的是工口本中的出了个美少女。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E4%BC%91%E5%88%8A%E5%B0%8F%E5%A7%90.jpg?raw=true"
+//           }, {
+//          CNtitle: "杀人乒乓球？《少年JUMP》推出乒乓球漫画新连载",
+//             CNdesc: "近日在《少年JUMP》上一部全新的乒乓球题材漫画开始连载，不过这部运动题材的漫画似乎是不走寻常路的，一上来就以不输战斗漫画的气势震惊了网友们。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E5%B0%91%E5%B9%B4jump.jpg?raw=true"
+//           }, {
+//          CNtitle: "漫画《人渣的本愿》即将开始连载外传故事",
+//             CNdesc: "根据微博网友发布的图片，在之前曾引发极大争议的《人渣的本愿》将于11月25日发售的《BGVol.12》上连载外传作品。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E4%BA%BA%E6%B8%A3%E7%9A%84%E6%9C%AC%E6%84%BF.jpg?raw=true"
+//           }, {
+//          CNtitle: "漫画《名侦探柯南》死了247人！官方公开十大领便当",
+//             CNdesc: "提起《名侦探柯南》，相信不少网友都会有死神小学生走到哪里杀人事件就发生在哪里的印象吧。而在最近官方发售的书籍《青山刚昌30周年本》中，官方正式公开了目前漫画版《名侦探柯南》中被杀死的被害人的数量。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E6%9F%AF%E5%8D%97.jpg?raw=true"
+//           }, {
+//          CNtitle: "漫画《金田一少年事件簿R》完结！《网球优等生》还有三话",
+//             CNdesc: "在本日发售的《周刊少年Magazine 46号》（讲谈社）上，由天树征丸原作，佐藤文也作画的漫画《金田一少年事件簿R》迎来了系列的最终话。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E9%87%91%E7%94%B0.jpg?raw=true"
+//           }, {
+//          CNtitle: "靠肌肉解决事件！日清推出网络漫画《名侦探吕布》",
+//             CNdesc: "日本食品品牌日清食品近日推出了网络漫画板块“NISSIN MANGA（日清漫画）”。根据日清透露，这次的漫画版块是为了让读者读到朝气蓬勃的作家们创作的漫画。而值得纪念的第一部漫画，就是《名侦探吕布》。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E5%90%8D%E4%BE%A6%E6%8E%A2%E5%90%95%E5%B8%83.jpg?raw=true"
+//           }, {
+//         CNtitle: "终于届到了！《好想告诉你》将在11月13日迎来最终话",
+//             CNdesc: "少女漫画总是容易被人吐槽主角感情发展细腻到急死读者，而这其中最出名的作品或许就是《好想告诉你》了。而最近，这部被戏称为《好想急死你》的作品也终于将在11月13日迎来连载的最终话。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E7%AA%81%E7%84%B6%E5%A5%BD%E6%83%B3%E4%BD%A0.jpg?raw=true"
+//           }, {
+//         CNtitle: " 漫画《花牌情缘》中学生篇连载开始！",
+//             CNdesc: "以歌留多为主题，讲述寻梦少女绫濑千早和同伴们的青春故事的漫画《花牌情缘》，从13日发售的漫画杂志《BE・LOVE》（讲谈社）21号上，开始连载在本篇中没有提到的主人公绫濑千早等人在中学时代的故事。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E8%8A%B1%E7%89%8C%E6%83%85%E7%BC%98.jpg?raw=true"
+//           }, {
+//          CNtitle: "日本杂志「月刊コミック@バンチ」刊登国漫《实验",
+//             CNdesc: "日本新潮社于9/21日所发售的「月刊コミック@バンチ」上公告：由超新星国人漫画家「Yanai」创作的《实验品家庭》，将于该杂志10/21日发售的12月号开始连载。",
+//             CNpicture: "https://github.com/FFFfire666/myPicture/blob/master/list-image/%E8%AF%95%E9%AA%8C%E5%93%81%E5%AE%B6%E5%BA%AD.jpg?raw=true"
+//           },
+//         ],
+//       //searchbar
+//       inputShowed: false,
+//       inputVal: ""
+//     },
+//     //searchbar
+//     showInput: function () {
+//         this.setData({
+//        inputShowed: true
+//           });
+//   },
+//   hideInput: function () {
+//       this.setData({
+//        inputVal: "",
+//           inputShowed: false
+//         });
+//   },
+//   clearInput: function () {
+//       this.setData({
+//        inputVal: ""
+//         });
+//   },
+//   inputTyping: function (e) {
+//       this.setData({
+//        inputVal: e.detail.value
+//         });
+//   },
+
+//     /**
+//     * 生命周期函数--监听页面加载
+//     */
+//     onLoad: function (options) {
+    
+//         },
+
+//     /**
+//     * 生命周期函数--监听页面初次渲染完成
+//     */
+//    onReady: function () {
+    
+//         },
+
+//     /**
+//     * 生命周期函数--监听页面显示
+//     */
+//    onShow: function () {
+    
+//       },
+
+//     /**
+//     * 生命周期函数--监听页面隐藏
+//     */
+//    onHide: function () {
+    
+//       },
+
+//     /**
+//     * 生命周期函数--监听页面卸载
+//     */
+//    onUnload: function () {
+    
+//         },
+
+//     /**
+//     * 页面相关事件处理函数--监听用户下拉动作
+//     */
+//    onPullDownRefresh: function () {
+    
+//         },
+
+//     /**
+//    * 页面上拉触底事件的处理函数
+//     */
+//     onReachBottom: function () {
+    
+//         },
+
+//     /**
+//    * 用户点击右上角分享
+//     */
+//     onShareAppMessage: function () {
+    
+//         }
+// }) 
