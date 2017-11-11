8b3409bf603336651d16942ec7a43d0112da9514
Zhankai <3325302163@qq.com>
2017-11-12 00:04:22 +0800

    #3 #88 第三次实验

---
+++ 1514080901135/pages/page4/page4.js
@@ -0,0 +1,24 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    array: [{
+      message: '../images/1.jpg',
+    },
+     {
+        message: '../images/2.jpg',
+      }, 
+      {
+        message: '../images/3.jpg',
+      },
+      {
+        message: '../images/4.jpg',
+      }
+      ],
+      value:true
+  },
+  //事件处理函数
+ 
+})
