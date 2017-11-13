e0b5fd527a7a28c30cc8327710b3eed3c0585955
zhanquan <542771826@qq.com>
2017-11-13 21:32:53 +0800

    #3 #68 第3次实验代码

---
+++ 1514080901131/pages/test/test.js
@@ -0,0 +1,19 @@
+Page({
+  data: {
+    list: [{
+      message: '../images/1.jpg',
+    },
+    {
+      message: '../images/2.jpg',
+    },
+    {
+      message: '../images/3.jpg',
+    },
+    {
+      message: '../images/4.jpg',
+    },
+    {
+      message: '../images/5.jpg',
+    }]
+  }
+})
