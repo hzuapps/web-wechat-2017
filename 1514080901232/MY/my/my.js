f0878c432646d094d5b2432c496ae131be9109b1
huangmaoliu <1404205899@qq.com>
2017-11-14 23:54:32 +0800

    #3 #72 第3次实验代码

--- 1514080901232/MY/my/my.js
+++ 1514080901232/MY/my/my.js
@@ -1,39 +1,106 @@
-Page({})
-App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+Page({
+  data: {
+    showTopTips: false,
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    radioItems: [
+      { name: 'cell standard', value: '0' },
+      { name: 'cell standard', value: '1', checked: true }
+    ],
+    checkboxItems: [
+      { name: 'standard is dealt for u.', value: '0', checked: true },
+      { name: 'standard is dealicient for u.', value: '1' }
+    ],
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    date: "2016-09-01",
+    time: "12:01",
+
+    countryCodes: ["+86", "+80", "+84", "+87"],
+    countryCodeIndex: 0,
+
+    countries: ["中国", "美国", "英国"],
+    countryIndex: 0,
+
+    accounts: ["微信号", "QQ", "Email"],
+    accountIndex: 0,
+
+    isAgree: false
+  },
+  showTopTips: function () {
+    var that = this;
+    this.setData({
+      showTopTips: true
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
+  },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
         }
       }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
     })
   },
-  globalData: {
-    userInfo: null
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindCountryCodeChange: function (e) {
+    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryCodeIndex: e.detail.value
+    })
+  },
+  bindCountryChange: function (e) {
+    console.log('picker country 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryIndex: e.detail.value
+    })
+  },
+  bindAccountChange: function (e) {
+    console.log('picker account 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      accountIndex: e.detail.value
+    })
+  },
+  bindAgreeChange: function (e) {
+    this.setData({
+      isAgree: !!e.detail.value.length
+    });
   }
-})
+});
--- 1514080901232/pages/index/index.js
+++ 1514080901232/pages/index/index.js
@@ -4,6 +4,17 @@ Page({
    * 页面的初始数据
    */
   data: {
+    urls: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510684207963&di=fe0519b0c26be7dfdfe7ca869c4211fd&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3Db43ca4169b8fa0ec6bca6c4e4efe339b%2F09fa513d269759eeeb8106eeb8fb43166c22dfc6.jpg', 
+      'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=848571865,3550609848&fm=200&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510684276393&di=3ca106c21320c9cd1f51352b6579837a&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3D4f59296a81d4b31ce4319cf8efbf4d0a%2F8601a18b87d6277f9b9b001122381f30e924fc09.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510684276392&di=fa27741be2315f59296d3325c2aa1f93&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dshijue1%252C0%252C0%252C294%252C40%2Fsign%3Dd9a09debeafe9925df0161135cc134aa%2Fbd315c6034a85edf6e387b0d43540923dd54756c.jpg'
+    ],
+    autoplay: true,
+    indicatordots: true,
+    duration: 1000,
+    interval: 2000,
+    circular: true
     
   },
 
@@ -12,11 +23,6 @@ Page({
    */
   onLoad: function (options) {
     
-  },
-  jumpToMyPage: function () {
-    wx.navigateTo({
-      url: '../yy/yy'
-    })
   },
 
   /**
