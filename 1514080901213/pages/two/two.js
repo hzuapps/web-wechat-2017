a421dedd22df344d87c572e5ef4805de441ca49e
252492046 <252492046>
2017-11-15 13:16:46 +0800

    #3 #22 第三次实验代码

--- 1514080901213/pages/two/two.js
+++ 1514080901213/pages/two/two.js
@@ -1,65 +1,34 @@
+var app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    //默认未获取地址
+    hasLocation: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  //获取经纬度
+  getLocation: function (e) {
+    console.log(e)
+    var that = this
+    wx.getLocation({
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          hasLocation: true,
+          location: {
+            longitude: res.longitude,
+            latitude: res.latitude
+          }
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  //根据经纬度在地图上显示
+  openLocation: function (e) {
+    var value = e.detail.value
+    wx.openLocation({
+      longitude: Number(value.longitude),
+      latitude: Number(value.latitude)
+    })
   }
 })
