3ddf6a990ac441eb13a8bc45c88632facac9a022
1369949044 <1369949044@qq.com>
2017-11-16 13:20:48 +0800

    #3 #32 第3次实验代码

--- 1514080901130/pages/index/index.js
+++ 1514080901130/pages/index/index.js
@@ -7,7 +7,53 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    list:[
+      {
+        id:'1',
+        name:"这是一个数组"
+      },
+      {
+        id: '2',
+        name: "这是一个数组"
+      },
+      {
+        id: '3',
+        name: "这是一个数组"
+      },
+      {
+        id: '4',
+        name: "这是一个数组"
+      },
+      {
+        id: '5',
+        name: "这是一个数组"
+      },
+      {
+        id: '6',
+        name: "这是一个数组"
+      },
+      {
+        id: '7',
+        name: "这是一个数组"
+      },
+      {
+        id: '8',
+        name: "这是一个数组"
+      },
+      {
+        id: '9',
+        name: "这是一个数组"
+      },
+      {
+        id: '10',
+        name: "这是一个数组"
+      },
+      {
+        id: '11',
+        name: "这是一个数组"
+      }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
