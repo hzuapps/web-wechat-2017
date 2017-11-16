433237c7bf97635fcb17f2b0ddbfdf8fe80ab1eb
CHEN <872673407@qq.com>
2017-11-16 11:49:37 +0800

    #3 #34 第3次实验报告

---
+++ 1514080901103/pages/scroll/scroll.js
@@ -0,0 +1,47 @@
+var tempNum = 0;
+Page({
+  data: {
+    meallist: [
+      {
+        Num: "1",
+        name: '默罕默德'
+      },
+      {
+        Num: "2",
+        name: '牛顿'
+      },
+      {
+        Num: "3",
+        name: '耶稣'
+      },
+      {
+        Num: "4",
+        name: '释迦牟尼'
+      },
+      {
+        Num: "5",
+        name: '孔丘'
+      },
+      {
+        Num: "6",
+        name: '保罗'
+      },
+      {
+        Num: "7",
+        name: '蔡伦'
+      },
+      {
+        Num: "8",
+        name: '古登堡'
+      },
+      {
+        Num: "9",
+        name: '哥伦布'
+      },
+      {
+        Num: "10",
+        name: '爱因斯坦'
+      }
+    ]
+  },
+})
