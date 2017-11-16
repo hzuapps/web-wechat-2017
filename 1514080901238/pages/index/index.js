da08bf249d56a908cca2360211593584894475ac
Your Name <903812136.com>
2017-11-16 09:04:49 +0800

    #3 #18 第三次实验代码修改版

--- 1514080901238/pages/index/index.js
+++ 1514080901238/pages/index/index.js
@@ -2,7 +2,14 @@ const pageData = {
     data: {
         taskList: [],
         task: '',
-        tipsHidden: true
+        tipsHidden: true,
+        tasklists:[{id:0,name:'做作业',time:'2017-10-11'},
+          { id: 1, name: '下午兼职', time: '2017-10-11' },
+          { id: 2, name: '做数学作业', time: '2017-10-11' },
+          {id: 3, name: '做语文作业', time: '2017-10-11' },
+          {id: 4, name: '做英语作业', time: '2017-10-11' },
+          { id: 4, name: '4点与闺蜜逛街', time: '2017-10-11' },
+          { id: 4, name: '19点陪爸妈吃完饭', time: '2017-10-11' }]
     },
 
     onShow: function () {
@@ -17,24 +24,24 @@ const pageData = {
         });
     },
 
-    toCreate: function () {
-        let taskList = this.data.taskList;
-        if (this.data.task.trim().length < 1) {
-            this.toShowTips();
-        } else {
-            taskList.unshift({
-                id: Date.now(),
-                text: this.data.task,
-                done: false
-            });
-            this.setData({
-                task: '',
-                taskList: taskList
-            });
-            wx.setStorageSync('taskList', taskList);
-        }
+    // toCreate: function () {
+    //     let taskList = this.data.taskList;
+    //     if (this.data.task.trim().length < 1) {
+    //         this.toShowTips();
+    //     } else {
+    //         taskList.unshift({
+    //             id: Date.now(),
+    //             text: this.data.task,
+    //             done: false
+    //         });
+    //         this.setData({
+    //             task: '',
+    //             taskList: taskList
+    //         });
+    //         wx.setStorageSync('taskList', taskList);
+    //     }
 
-    },
+    // },
 
     toggleDone: function (e) {
         let id = e.currentTarget.id;
