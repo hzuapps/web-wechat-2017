9b01139d01055d8abbb99254e8cb9f067ae73ace
hongdi178 <183164746@qq.com>
2017-11-17 10:44:29 +0800

    #3 #17 第三次实验实验代码

--- 1507070201226/app.js
+++ 1507070201226/app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+  
     // 登录
     wx.login({
       success: res => {
@@ -33,7 +33,4 @@ App({
       }
     })
   },
-  globalData: {
-    userInfo: null
-  }
 })
