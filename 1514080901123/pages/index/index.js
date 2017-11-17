4664f05f60349d45438dcc4e09229733f75c7fac
panweil <762178895@qq.com>
2017-11-17 10:08:51 +0800

    #3 #23  可滚动界面的代码

--- 1514080901123/pages/index/index.js
+++ 1514080901123/pages/index/index.js
@@ -2,33 +2,48 @@ Page({
   data: {
     grids: [{
       name: '世界记录',
-      icon: '../example/images/index/1.png'
+      icon: '../example/images/index/1.png',
+      url: '../jilu/WR'
+    }, 
+    {
+      name: '中国记录',
+      icon: '../example/images/index/2.png',
+      url: '../jilu/NR'
     },
     {
       name: '公式库',
-      icon: '../example/images/index/2.png',
+      icon: '../example/images/index/3.png',
       url: '../gongshi/gongshi'
     },
     {
       name: '视频教程',
-      icon: '../example/images/index/3.png',
-      url: '../jiaocheng/jiaocheng'
+      icon: '../example/images/index/4.png',
+      url: '../jiaocheng/index'
     },
     {
       name: '魔方品牌',
-      icon: '../example/images/index/4.png'
+      icon: '../example/images/index/5.png',
+      url: '../rubikbrand/brand'
     },
     {
       name: '近期赛事',
-      icon: '../example/images/index/5.png'
+      icon: '../example/images/index/6.png',
+      url: '../match/match'
     },
     {
       name: '趣味统计',
-      icon: '../example/images/index/6.png'
+      icon: '../example/images/index/7.png',
+      url: '../tongji/tongji'
     },
     {
       name: '各项国内前十',
-      icon: '../example/images/index/7.png'
+      icon: '../example/images/index/8.png',
+      url: '../top10/top10'
+    },
+    {
+      name: '计时器',
+      icon: '../example/images/index/10.png',
+      url: '../timer/timer'
     }
     ]
   }
