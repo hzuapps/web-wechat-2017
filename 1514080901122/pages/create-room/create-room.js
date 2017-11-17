445c27559279e2a11b74191ee208e8f1a567e615
AllenMinD <240831608@qq.com>
2017-11-17 11:33:56 +0800

    #4 #9 第4次实验代码

--- 1514080901122/pages/create-room/create-room.js
+++ 1514080901122/pages/create-room/create-room.js
@@ -4,62 +4,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    times: ["2分钟", "3分钟", "5分钟"],
+    timeIndex: 0,
+    startNum: null,
+    endNum: null,
+    limitTime: null
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  bindTimeChange: function (e) {
+    //console.log('picker account 发生选择改变，携带值为', e.detail.value);
+    this.setData({
+      timeIndex: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    if (e.detail.value.startNum > e.detail.value.endNum) {
+      console.log("起始学号必须小于终止学号。");
+    } else {
+      this.setData({
+        startNum: e.detail.value.startNum,
+        endNum: e.detail.value.endNum,
+        limitTime: e.detail.value.limitTime
+      });
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
+  formReset: function () {
+    console.log('form发生了reset事件')
+  }  
 })
