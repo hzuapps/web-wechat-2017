ead15b4aceafdd0395f0960bb372f9b01ed7e5db
wmingl <1310259682@qq.com>
2017-11-17 19:08:55 +0800

    #3 #75 第3次实验代码

--- 1513071001131/pages/index/index.js
+++ 1513071001131/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-   
+  
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -13,6 +13,11 @@ Page({
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  jumpTokaishi: function () {
+    wx.navigateTo({
+      url: '../kaishi/kaishi'
     })
   },
   onLoad: function () {
