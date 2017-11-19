dccf6163ecfd97cbb5e68901bd3199197b9f6d61
cuteboyjun <471054396@qq.com>
2017-11-19 20:42:42 +0800

    #3 #154  第3次实验代码

--- 1514080901231/pages/logs/logs.js
+++ 1514080901231/pages/logs/logs.js
@@ -1,15 +1,52 @@
-//logs.js
-const util = require('../../utils/util.js')
-
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
+  /**
+   * 页面的初始数据
+   */
+    data: {
+    ListLi:[],
+      page: 0,
+      isLower : false, //是否已滑动到底部
+    },
+  
+    //初始化数据
+    initData: function () {
+        var _lis = [];
+        for (var i = 0; i < 20; i++) {
+            _lis.push("30  肥胖");
+          }
+        this.setData({
+      listLi: _lis,
+            page: 1
+          })
+    },
+
+    /**
+   * 生命周期函数--监听页面加载
+   */
+    onLoad: function (options) {
+          var that = this;
+        try {
+            //初始化数据
+              this.initData();
+          } catch (e) { console.log(e.message) }
+        console.log(this.data.listLi);
+      },
+
+    /**
+   * 滑动到底部时
+   */
+    lower:function() {
+        console.log("已滑动到底部")
+       let _page = this.data.page + 1;
+        let _lis = this.data.listLi;
+      //滑动到底部时，继续向下滑动则继续加载数据
+        for(var i = (_page - 1) * 20; i < _page * 20; i++) {
+          _lis.push("30  肥胖");
+           }
+      this.setData({
+    listLi: _lis,
+          page: _page,
+          isLower: true,
+        })
   }
-})
+}) 
