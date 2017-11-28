15814460faba8a185350e5a08c7027ec191120fd
yuezhudxw <2495124383@qq.com>
2017-11-28 22:15:02 +0800

    #3 #59 第3次实验代码

--- 1514080901108/pages/index/index.js
+++ 1514080901108/pages/index/index.js
@@ -4,10 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    images:["1","2","3","4","5","6","7","8","9","10"]
   },
   //事件处理函数
   bindViewTap: function() {
