86ae8f5754598cd20218fd6d869cfadadc7fa614
panweil <762178895@qq.com>
2017-11-30 08:26:11 +0800

    #4 #23  第4次实验增加表单

---
+++ 1514080901123/pages/me/me.js
@@ -0,0 +1,50 @@
+
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {}
+
+  },
+  //事件处理函数
+  
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      wx.setStorage({
+        key: "userinfo",
+        data: app.globalData.userInfo})
+
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo
+          })
+        }
+      })
+    }
+  },
+  /*getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }*/
+})
