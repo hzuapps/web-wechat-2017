a3d5575fb8cbdadd30a7404fa8ec5dd3a08d5bd8
AllenMinD <240831608@qq.com>
2017-11-30 09:36:47 +0800

    #5 #9 第5次实验代码

--- 1514080901122/pages/student-list/student-list.js
+++ 1514080901122/pages/student-list/student-list.js
@@ -1,33 +1,22 @@
 var tempNum = 0;
 Page({
   data: {
-    students: [
-      {
-        sNum: "1122",
-        name:'比尔盖茨'
-      }, 
-      {
-        sNum: "1124",
-        name:'扎克伯格'
-      }, 
-      {
-        sNum: "1136", 
-        name:'老王' 
-      }, 
-      {
-        sNum: "1148", 
-        name: '老李'
-      }, 
-      {
-        sNum: "1155",
-        name:'杨赵'
-      },
-      {
-        sNum: "5155",
-        name: '呵呵大'
-      }
-    ]
+    students: []
   },
+
+  onLoad: function(options) {
+    var that = this;
+    wx.request({
+      url: "http://www.easy-mock.com/mock/5a1f5ba9ca645757c0517e26/wechat-app-test/student-list",
+      success: function (res) {
+        var resData = res.data.data;
+        that.setData({
+          students: resData
+        });
+      }
+    });
+  },
+
   more: function (e) {
     console.log(e);
     tempNum++;
