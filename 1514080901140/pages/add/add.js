2fca2bce70549750cf11d7d6ad75d7606796b057
wagez <1103590804@qq.com>
2017-12-01 10:35:13 +0800

    #4 #13 第4次实验代码

---
+++ 1514080901140/pages/add/add.js
@@ -0,0 +1,138 @@
+// pages/add/add.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    colleage: 'xx学院',
+    department: 'xx部门',
+    pnumber: '13623332333',
+    area: 'call',
+    count: 'call'.length
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.getStorage({
+      key: 'input',
+      success: function (res) {
+        console.log(res.data.area.length)
+
+        that.setData({
+          text: res.data.text,
+          area: res.data.area,
+          count: res.data.area.length
+        })
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onSubmit: function (event) {
+    console.dir(event)
+    var that = this
+    var text = event.detail.value.text
+    if (!text) {
+      console.dir(text)
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+      wx.setStorage({
+        key: "input",
+        data: event.detail.value,
+        success: function (res) {
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000
+          })
+
+        }
+      })
+    }
+  },
+
+  onTextChange: function (e) {
+    var that = this;
+    console.dir(e)
+    var text = e.detail.value
+    console.dir(text);
+    if (!text) {
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+    }
+  },
+
+  onChange: function (e) {
+    var that = this;
+    var value = e.detail.value;
+    console.dir(value)
+    that.setData({
+      count: value.length
+    })
+  }
+})
