7412210474e0a20ecf084c8b1b6712345bba276e
JMLUO <292656342@qq.com>
2017-12-01 10:39:56 +0800

    #4 #12 第4次实验代码

---
+++ 1514080901127/pages/mine/mine.js
@@ -0,0 +1,173 @@
+Page({
+  data: {
+    showTopTips: false,
+
+    radioItems: [
+      { name: '男', value: '0' },
+      { name: '女', value: '1', checked: true }
+    ],
+    checkboxItems: [
+      { name: '日常账本', value: '0', checked: true },
+      { name: '生意账本', value: '1' }
+    ],
+
+    date: "2017-12-01",
+    time: "09:00",
+
+    countryCodes: ["+86", "+80", "+84", "+87"],
+    countryCodeIndex: 0,
+
+    countries: ["中国", "美国", "英国"],
+    countryIndex: 0,
+
+    accounts: ["微信", "QQ", "手机号码"],
+    accountIndex: 0,
+
+    isAgree: false
+  },
+  showTopTips: function () {
+    var that = this;
+    this.setData({
+      showTopTips: true
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
+  },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindCountryCodeChange: function (e) {
+    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryCodeIndex: e.detail.value
+    })
+  },
+  bindCountryChange: function (e) {
+    console.log('picker country 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryIndex: e.detail.value
+    })
+  },
+  bindAccountChange: function (e) {
+    console.log('picker account 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      accountIndex: e.detail.value
+    })
+  },
+
+  onLoad: function (options) {
+    var that = this;
+    wx.getStorage({
+      key: 'input',
+      success: function (res) {
+        console.log(res.data.area.length)
+
+        that.setData({
+          text: res.data.text,
+          area: res.data.area,
+          count: res.data.area.length
+        })
+      }
+    })
+  },
+  onSubmit: function (event) {
+    console.dir(event)
+    var that = this
+    var text = event.detail.value.text
+    if (!text) {
+      console.dir(text)
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+      wx.setStorage({
+        key: "input",
+        data: event.detail.value,
+        success: function (res) {
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000
+          })
+
+        }
+      })
+    }
+  },
+
+  onTextChange: function (e) {
+    var that = this;
+    console.dir(e)
+    var text = e.detail.value
+    console.dir(text);
+    if (!text) {
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+    }
+  },
+
+
+
+  onChange: function (e) {
+    var that = this;
+    var value = e.detail.value;
+    console.dir(value)
+    that.setData({
+      count: value.length
+    })
+  }
+});
