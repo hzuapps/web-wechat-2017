c38298fb74a0fdf4e8261563908b3aebbf5a68a1
Buruiwen <1220269525@qq.com>
2017-12-05 17:59:34 +0800

    #4 #54 第四次实验代码

--- 1509081602501/pages/index/index.js
+++ 1509081602501/pages/index/index.js
@@ -1,146 +1,77 @@
-//index.js
-//获取应用实例
-const app = getApp()
+var app = getApp();
 Page({
   data: {
-    scrollTop: 100,
-    array: [{
-      msg:'你好',
-      time:'yesterday',
-      text:'Anna',
-      icon:'../../image/1.jpg'
-    },{
-        msg: '在吗',
-        time: 'today',
-      text: 'Bill',
-      icon: '../../image/2.jpg'
-    }, {
-        msg: '请问你',
-        time: 'Sun',
-      text: 'Klia',
-      icon: '../../image/3.jpg'
-    }, {
-        msg: '你好',
-        time: '11/17/12',
-        text: 'Home',
-      icon: '../../image/4.jpg'
-    }, {
-        msg: '吃饭了吗',
-        time: 'yesterday',
-        text: 'Koli',
-      icon: '../../image/5.jpg'
-    },
-    {
-      msg: '好久不见',
-      time: '8:45AM',
-      text: 'Jkil',
-      icon: '../../image/6.jpg'
-    },
-    {
-      msg: '你好',
-      time: 'yesterday',
-      text: 'Fiki',
-      icon: '../../image/1.jpg'
-    },
-    {
-      msg:' 哈哈哈',
-      time: '9:00PM',
-      text: 'Sili',
-      icon: '../../image/2.jpg'
-    },
-    {
-      msg: '见到你真高兴',
-      time: 'yesterday',
-      text: 'Dava',
-      icon: '../../image/3.jpg'
-    },
-    
-    ],
+    // text:"这是一个页面"  
+    array: ["中国", "美国", "巴西", "日本"],
+    toast1Hidden: true,
+    modalHidden: true,
+    modalHidden2: true,
+    notice_str: '',
+    index: 0
   },
-  imageError: function (e) {
-    console.log('image3发生error事件，携带值为', e.detail.errMsg)
+  toast1Change: function (e) {
+    this.setData({ toast1Hidden: true });
   },
-
-  onLoad: function () {
-
-  },
-    onShow: function () {
-
-  },
-
-    onHide: function(){
-
-    },
-    onUnload:function(){
-
-    },
-
-    onReachBottom:function(){
-
-    },
-
-    loadMore:function () {
-      var that=this;
-      wx.showLoading({
-        title:"正在加载..."
-      })
-      that.setData({
-        array: that.data.array.concat
-          ([{
-            msg: '你好',
-            time: 'yesterday',
-            text: 'Anna',
-            icon: '../../image/1.jpg'
-          }, {
-            msg: '在吗',
-            time: 'today',
-            text: 'Bill',
-            icon: '../../image/2.jpg'
-          }, {
-            msg: '请问你',
-            time: 'Sun',
-            text: 'Klia',
-            icon: '../../image/3.jpg'
-          }, {
-            msg: '你好',
-            time: '11/17/12',
-            text: 'Home',
-            icon: '../../image/4.jpg'
-          }, {
-            msg: '吃饭了吗',
-            time: 'yesterday',
-            text: 'Koli',
-            icon: '../../image/5.jpg'
-          },
-          {
-            msg: '好久不见',
-            time: '8:45AM',
-            text: 'Jkil',
-            icon: '../../image/6.jpg'
-          },
-          {
-            msg: '你好',
-            time: 'yesterday',
-            text: 'Fiki',
-            icon: '../../image/1.jpg'
-          },
-          {
-            msg:'哈哈哈',
-            time: '9:00PM',
-            text: 'Sili',
-            icon: '../../image/2.jpg'
-          },
-          {
-            msg: '见到你真高兴',
-            time: 'yesterday',
-            text: 'Dava',
-            icon: '../../image/3.jpg'
-          },
-        ])
+  //弹出确认框  
+  modalTap: function (e) {
+    this.setData({
+      modalHidden: false
     })
-    setTimeout(function(){
-      wx.hideLoading()
-    },1000)
-
-    }
+  },
+  confirm_one: function (e) {
+    console.log(e);
+    this.setData({
+      modalHidden: true,
+      toast1Hidden: false,
+      notice_str: '提交成功'
+    });
+  },
+  cancel_one: function (e) {
+    console.log(e);
+    this.setData({
+      modalHidden: true,
+      toast1Hidden: false,
+      notice_str: '取消成功'
+    });
+  },
+  //弹出提示框  
+  modalTap2: function (e) {
+    this.setData({
+      modalHidden2: false
+    })
+  },
+  modalChange2: function (e) {
+    this.setData({
+      modalHidden2: true
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数  
+  },
+  onReady: function () {
+    // 页面渲染完成  
+  },
+  onShow: function () {
+    // 页面显示  
+  },
+  onHide: function () {
+    // 页面隐藏  
+  },
+  onUnload: function () {
+    // 页面关闭  
+  },
+  formSubmit: function (e) {
+    console.log('form发生了submit事件');
+    this.modalTap();
+  },
+  formReset: function () {
+    console.log('form发生了reset事件');
+    this.modalTap2();
+  }
 })
