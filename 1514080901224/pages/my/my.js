a3d02f25cd436ffc4a2c6e7fe179a88dab886c97
bigbigban <1372708162@qq.com>
2017-12-05 12:44:38 +0800

    #4  #184  第4次实验代码

--- 1514080901224/pages/my/my.js
+++ 1514080901224/pages/my/my.js
@@ -1,125 +1,150 @@
-// pages/demo/scrollView.js
 Page({
 
   /**
-  * 页面的初始数据
-  */
+   * 页面的初始数据
+   */
   data: {
-    session: []
+    text: '',
+    area: '',
   },
 
   /**
-  * 生命周期函数--监听页面加载
-  */
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
-    var that = this
-    // 设置窗口大小
-    wx.getSystemInfo({
-      success: (res) => {
+    var that = this;
+    wx.getStorage({
+      key: 'input',
+      success: function (res) {
+        console.log(res.data.area.length)
+
         that.setData({
-          windowHeight: res.windowHeight - 5,
-          windowWidth: res.windowWidth
+          text: res.data.text,
+          area: res.data.area,
+          count: res.data.area.length
         })
-        //console.dir(that.data.windowHeight)
       }
     })
-    that.setData({
-      sessions: [{
-        name: "",
-        icon: "",
-        msg: ""
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }, {
-        name: "J",
-        icon: "",
-        msg: ""
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }]
+    // 从服务器取回来 JSON
+    wx.request({
+      url: 'https://infoaas.com/data/hzc.json',
+      //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        /*
+        that.setData({
+          hasError: true,
+          errorText: res.data.name + ',' + res.data.teacher + ' ' + res.data.year
+        })*/
+      }
     })
   },
 
   /**
-  * 生命周期函数--监听页面初次渲染完成
-  */
+   * 生命周期函数--监听页面初次渲染完成
+   */
   onReady: function () {
+
   },
 
   /**
-  * 生命周期函数--监听页面显示
-  */
+   * 生命周期函数--监听页面显示
+   */
   onShow: function () {
+
   },
 
   /**
-  * 生命周期函数--监听页面隐藏
-  */
+   * 生命周期函数--监听页面隐藏
+   */
   onHide: function () {
+
   },
 
   /**
-  * 生命周期函数--监听页面卸载
-  */
+   * 生命周期函数--监听页面卸载
+   */
   onUnload: function () {
+
   },
 
   /**
-  * 页面相关事件处理函数--监听用户下拉动作
-  */
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
   onPullDownRefresh: function () {
+
   },
 
   /**
-  * 页面上拉触底事件的处理函数
-  */
+   * 页面上拉触底事件的处理函数
+   */
   onReachBottom: function () {
+
   },
 
   /**
-  * 用户点击右上角分享
-  */
+   * 用户点击右上角分享
+   */
   onShareAppMessage: function () {
+
   },
 
-  onEnd: function (e) {
-    console.dir(e)
+  onSubmit: function (event) {
+    console.dir(event)
+    var that = this
+    var text = event.detail.value.text
+    if (!text) {
+      console.dir(text)
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+      wx.setStorage({
+        key: "input",
+        data: event.detail.value,
+        success: function (res) {
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000
+          })
+
+        }
+      })
+    }
+  },
+
+  onTextChange: function (e) {
     var that = this;
-    that.setData({
-      //isLower: true,
-      sessions: that.data.sessions.concat([{
-        name: "",
-        icon: "",
-        msg: "。"
-      }, {
-        name: "",
-        icon: "",
-        msg: ""
-      }])
-    })
+    console.dir(e)
+    var text = e.detail.value
+    console.dir(text);
+    if (!text) {
+      that.setData({
+        hasError: true,
+        errorText: ''
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+    }
   },
 
-  onScroll: function () {
-    console.dir("onScroll");
+  onChange: function (e) {
+    var that = this;
+    var value = e.detail.value;
+    console.dir(value)
+    that.setData({
+      count: value.length
+    })
   }
 })
