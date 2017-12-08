64d017256013da5588c18ed5148a20b40cefefdb
cuteboyjun <471054396@qq.com>
2017-12-08 22:08:16 +0800

    #5  #154  第5次实验代码

--- 1514080901231/pages/index/index.js
+++ 1514080901231/pages/index/index.js
@@ -1,54 +1,143 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    text: '',
+    area: '',
+    count: 'world'.length
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.getStorage({
+      key: 'input',
+      success: function (res) {
+        console.log(res.data.area.length)
+
+        that.setData({
+          text: res.data.text,
+          area: res.data.area,
+          count: res.data.area.length
         })
       }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: 'BMI指数计算器',
+      desc: '随时随地查看BMI指数',
+      path: '/page/user?id=123',
+      success: function (res) {
+        // 转发成功
+      },
+      fail: function (res) {
+        // 转发失败
+      }
+    }
+  },
+
+  onSubmit: function (event) {
+    console.dir(event)
+    var that = this
+    var text = event.detail.value.text
+    if (!text) {
+      console.dir(text)
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+      that.setData({
+        hasError: false
+      })
+      wx.setStorage({
+        key: "input",
+        data: event.detail.value,
+        success: function (res) {
+          wx.showToast({
+            title: '成功',
+            icon: 'success',
+            duration: 2000
           })
+
         }
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  onTextChange: function (e) {
+    var that = this;
+    console.dir(e)
+    var text = e.detail.value
+    console.dir(text);
+    if (!text) {
+      that.setData({
+        hasError: true,
+        errorText: '文字不能为空！'
+      })
+    } else {
+      that.setData({
+        hasError: false
+      })
+    }
+  },
+
+  onChange: function (e) {
+    var that = this;
+    var value = e.detail.value;
+    console.dir(value)
+    that.setData({
+      count: value.length
     })
   }
 })
