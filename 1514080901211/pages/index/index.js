9567e72a6f099840f7a396599e0646472e62e946
hhhhjq <1030623463@qq.com>
2017-12-12 15:18:13 +0800

    #5 #102 第5次实验代码

--- 1514080901211/pages/index/index.js
+++ 1514080901211/pages/index/index.js
@@ -1,54 +1,23 @@
-//index.js
-//获取应用实例
 const app = getApp()
-
-Page({
+  
+  Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+     result: []
+      },
+
+    onLoad: function () {
+       var that = this;
+        wx.request({
+       url: 'https://infoaas.com/data/hzc.json',
+            method: 'GET',
+            header: {
+         'content-type': 'application/json'
+            },
+      success: function (res) {
+          that.setData({
+           result: res.data
+           })
+     }
     })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
