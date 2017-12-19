ad7c50c27cf79e2e8464b6c0054ce010fd49472b
252492046 <252492046>
2017-12-19 18:26:04 +0800

    #5 #22 第5次实验代码

--- 1514080901213/pages/three/three.js
+++ 1514080901213/pages/three/three.js
@@ -1,3 +1,4 @@
+/*
 Page({
   data:{
     shuju:["456","789"]
@@ -10,5 +11,12 @@ Page({
       shuju: this.data.shuju
     })
 
+  }
+})
+*/
+Page({
+  data:{
+    //imageUrl: "http://img1.3lian.com/2015/w7/85/d/101.jpg"
+    imageUrl: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1512106169564&di=e6ef9d0a9bae9c7208849b0ae138bfde&imgtype=jpg&src=http%3A%2F%2Fimg1.imgtn.bdimg.com%2Fit%2Fu%3D4260769926%2C1262352686%26fm%3D214%26gp%3D0.jpg"
   }
 })
