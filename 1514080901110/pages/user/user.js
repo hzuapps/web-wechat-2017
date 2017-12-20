58f44245f15113e29f6d8dd557fa92cef07f7c0b
Leezxy <963394524@qq.com>
2017-12-20 08:35:16 +0800

    #5 #166 第五次实验代码

--- 1514080901110/pages/user/user.js
+++ 1514080901110/pages/user/user.js
@@ -1,8 +1,8 @@
-﻿var tempNum = 0;
+var tempNum = 0;
 Page({
   data: {
    days: [
-      {
+      /*{
         w: "星期一",
         s:"等我会让你看到满天繁星"
       
@@ -30,6 +30,24 @@ Page({
       {
         w: "星期日",
         s: "等我会让你看到满天繁星"
-      }    ]
+      } */   ]
+  },
+  onLoad: function () {
+    var that = this;
+    wx.request({
+      url: 'https://infoaas.com/data/1514080901128/ComicNew.json',
+      //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        //console.log(res.data.list)
+        that.setData({
+          hasError: true,
+          ranks: res.data.list
+        })
+      }
+    })
   },
  })
