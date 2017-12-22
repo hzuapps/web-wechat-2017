b4a298f5216dbca1969e0ab6b855ca4ead51a1e3
bigbigban <1372708162@qq.com>
2017-12-23 00:41:32 +0800

    #5 #184 第5次实验代码

--- 1514080901224/pages/index/index.js
+++ 1514080901224/pages/index/index.js
@@ -1,54 +1,101 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    items: [],
+    hidden: false,
+    loading: false,
+    loadmorehidden: true,
+    plain: false
   },
-  //事件处理函数
-  bindViewTap: function() {
+
+  onItemClick: function (event) {
+    var targetUrl = "/pages/images";
+    if (event.currentTarget.dataset.url != null)
+      targetUrl = targetUrl + "?url=" + event.currentTarget.dataset.url;
     wx.navigateTo({
-      url: '../logs/logs'
-    })
+      url: targetUrl
+    });
   },
+
+  loadMore: function (event) {
+    var that = this
+    requestData(that, mCurrentPage + 1);
+  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    console.log('onLoad')
+    var that = this
+    requestData(that, mCurrentPage + 1);
   }
 })
+
+/**
+ * 定义几个数组用来存取item中的数据
+ */
+var mUrl = [];
+var mDesc = [];
+var mWho = [];
+var mTimes = [];
+var mTitles = [];
+
+var mCurrentPage = 0;
+
+
+/**
+ * 请求数据
+ * @param that Page的对象，用来setData更新数据
+ * @param targetPage 请求的目标页码
+ */
+function requestData(that, targetPage) {
+  wx.request({
+    url: 'http://image.baidu.com/data/imgs?col=美女&tag=小清新&sort=0&pn=10&rn=10&p=channel&from=1',
+
+    success: function (res) {
+      if (res == null ||
+        res.data == null ||
+        res.data.results == null ||
+        res.data.results.length <= 0) {
+
+       
+        return;
+      }
+
+      var i = 0;
+      for (; i < res.data.results.length; i++)
+        bindData(res.data.results[i]);
+
+      //将获得的各种数据写入itemList，用于setData
+      var itemList = [];
+      for (var i = 0; i < mUrl.length; i++)
+        itemList.push({ url: mUrl[i], desc: mDesc[i], who: mWho[i], time: mTimes[i], title: mTitles[i] });
+
+      that.setData({
+        items: itemList,
+        hidden: true,
+        loadmorehidden: false,
+      });
+
+      mCurrentPage = targetPage;
+    }
+  });
+}
+
+/**
+ * 绑定接口中返回的数据
+ * @param itemData Gank.io返回的content;
+ */
+function bindData(itemData) {
+
+  var url = itemData.url.replace("//ww", "//ws");
+  var desc = itemData.desc;
+  var who = itemData.who;
+  var times = itemData.publishedAt.split("T")[0];
+
+  mUrl.push(url);
+  mDesc.push(desc);
+  mWho.push(who);
+  mTimes.push(times);
+  mTitles.push(times);
+}
