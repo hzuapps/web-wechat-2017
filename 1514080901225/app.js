ad2bb091588f8ac69bcf701480adb1cdcc7d556d
Reubon <Reubon043@163.com>
2017-12-28 13:43:29 +0800

    #5 #93 第5次实验代码

--- 1514080901225/app.js
+++ 1514080901225/app.js
@@ -2,13 +2,15 @@ App({
   appData(){
     userinfo:null
   },
+  globalData: {
+    userInfo: null
+  },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
     
   },
-
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
--- 1514080901225/pages/login/login.js
+++ 1514080901225/pages/login/login.js
@@ -7,13 +7,46 @@ Page({
   data: {
     username:null,
     password:null,
+    warn:null,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+
+    }
+    else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    }
+    else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
@@ -65,20 +98,43 @@ Page({
 
 
   Login : function () {
-    app.appData.userinfo = { username: this.data.username, password: this.data.password, },
-    wx.switchTab({
-      url: "../mine/mine",
-    })
-    console.log(app.appData.userinfo.username)
+    if(this.data.username==null||this.data.password==null){
+      this.setData({ warn: '请输入正确的用户名和密码！' })
+      console.log(this.data.username)
+      console.log(this.data.password)
+    }
+    else{
+      app.appData.userinfo = { username: this.data.username, password: this.data.password, }
+      wx.switchTab({ url: "../mine/mine", })
+    }
   },
 
 
   usernameInput : function(event){
     this.setData({username:event.detail.value})
+    if (event.detail.value == '')
+      this.setData({ username: null })
+    console.log(this.data.username)
   }, 
 
 
   passwordInput : function (event) {
     this.setData({password: event.detail.value})
+    if (event.detail.value=='') 
+       this.setData({password:null})
+    console.log(this.data.password)
+  },
+  
+  
+  getUserInfo: function (e) {
+    console.log(e)
+    var nickname = e.detail.userInfo.nickName
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+    app.appData.userinfo = { username: nickname, password: this.data.password, }
+    wx.switchTab({ url: "../mine/mine", })
   }
 })
