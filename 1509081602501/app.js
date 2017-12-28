541cd3cac2c877de399ba9776f5693833dcc8668
Buruiwen <1220269525@qq.com>
2017-12-28 09:14:22 +0800

    #6 #54 第6次实验代码

--- 1509081602501/app.js
+++ 1509081602501/app.js
@@ -1,39 +1,61 @@
-//app.js
+import {log, promiseHandle} from 'utils/util';
+import locales from './utils/locales'
+import T from './utils/wxapp-i18n'
+
+T.registerLocale(locales)
+T.setLocale('zh-Hans')
+wx.T = T
+
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+  onLaunch() {
+    this.clearStorageAfterUpdated()
+  },
+  clearStorageAfterUpdated() {
+    wx.getStorage({
+      key: 'version',
+      success: (res) => {
+        if (res.data !== this.globalData.version) {
+          wx.clearStorage()
+          this.setVersion()
         }
-      }
+      },
+      fail: (res) => {
+        this.setVersion()
+      },
     })
   },
+  setVersion() {
+    wx.setStorage({
+      key: 'version',
+      data: this.globalData.version,
+    })
+  },
+
+  getUserInfo(cb) {
+    if (typeof cb !== "function") return;
+    let that = this;
+    if (that.globalData.userInfo) {
+      cb(that.globalData.userInfo);
+    } else {
+      promiseHandle(wx.login).then(() => promiseHandle(wx.getUserInfo)).then(res => {
+        that.globalData.userInfo = res.userInfo;
+        cb(that.globalData.userInfo);
+      }).catch(err => {
+        log(err);
+      });
+    }
+  },
+
   globalData: {
     userInfo: null
+  },
+
+  //自定义配置
+  settings: {
+    debug: true, //是否调试模式
+    moreLink: 'http://github.com/oopsguy',
+    version: "1.1.0",
+    env: "prod",
+
   }
-})
+});  
