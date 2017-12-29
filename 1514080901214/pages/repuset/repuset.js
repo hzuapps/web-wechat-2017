44d93e0c26a2b998398390b8969f929c276c507f
JIZHILING <736443319@qq.com>
2017-12-29 16:34:17 +0800

    #6 #55 第6次实验

--- 1514080901214/pages/repuset/repuset.js
+++ 1514080901214/pages/repuset/repuset.js
@@ -1,108 +1,31 @@
+//index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    nr: {
-      text: "",
-      area: ""
-    }
-
-
+    title: "加载中...",
+    duanzi: [],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function () {
-    //    var nr = wx.getStorageSync('key')
-    //    var that = this
-    //    that.setData({
-    //     nr: {
-    //       text: nr.text,
-    //      area: nr.area
-    //     }
-    //   });
+    var that = this;
+    wx.showToast({
+      title: '加载中...',
+      icon: 'loading',
+      duration: 1000,
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  formSubmit: function (e) {
-    //   console.log(e.detail.value);
-    var that = this
-    //   that.setData({
-    //   nr:{
-    //     text: e.detail.value.text,
-    //     area: e.detail.value.area
-    //   }
-    //  }),
-    //    wx.setStorage({
-    //      key: "key",
-    //     data: e.detail.value
-    //   })
     wx.request({
-      url: 'https://infoaas.com/data/1514080901101/eat.json', //仅为示例，并非真实的接口地址
+      url: "https://route.showapi.com/341-1?showapi_appid=52772&showapi_sign=b194edf7f7954d7384666ae477c42fe6&page=1&maxResult=50&",
       header: {
-        'content-type': 'application/json' // 默认值
+        "Content-Type": "json"
       },
       success: function (res) {
+        // wx.hideToast();
+        var data = res.data;
+        console.log(data);
         that.setData({
-          nr: res.data.nr,
+          duanzi: data.showapi_res_body.contentlist,
         })
-        console.log(res.data)
-      }
+      },
     })
-  },
-  formReset: function () {
-    console.log('form发生了reset事件')
+
   }
 })
