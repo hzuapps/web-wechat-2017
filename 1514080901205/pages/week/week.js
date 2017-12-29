d7b4f912d9da8492fc071869f8a579aa0eea45a9
JIZHILING <736443319@qq.com>
2017-12-29 14:38:54 +0800

    #6 #41 第6次实验代码

---
+++ 1514080901205/pages/week/week.js
@@ -0,0 +1,31 @@
+Page({
+
+  data: {
+    title: "加载中...",
+    ri: []
+  },
+  onLoad: function () {
+    var that = this;
+    wx.showToast({
+      title: '加载中...',
+      icon: 'loading',
+      duration: 1000,
+    })
+
+    wx.request({
+      url: "https://route.showapi.com/578-1?showapi_appid=49955&showapi_sign=9911fe96a0e747b79ae822f20fedaede&",
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) {
+        // wx.hideToast();
+        var data = res.data;
+        //console.log(data);
+        that.setData({
+          ri: data.showapi_res_body.datalist
+        })
+      },
+    })
+
+  }
+})
