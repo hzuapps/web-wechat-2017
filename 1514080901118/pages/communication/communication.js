cb5c580df4bbab2b1b47e441a778cc7e123a1fc0
[ldy12347] <[986631239@qq.com]>
2017-12-31 15:45:17 +0800

    第六次实验代码

---
+++ 1514080901118/pages/communication/communication.js
@@ -0,0 +1,169 @@
+Page({
+  data: {
+    // text:"这是一个页面"  
+    toast1Hidden: true,
+    modalHidden: true,
+    modalHidden2: true,
+    notice_str: '',
+    index: 0,
+    showTopTips: false,
+    countries: ["广州", "惠州", "汕尾", "河源", "深圳", "潮州"],
+    countryIndex: 0,
+
+    isAgree: false
+  },
+  showTopTips: function () {
+    var that = this;
+    this.setData({
+      showTopTips: true
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
+  },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindCountryCodeChange: function (e) {
+    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryCodeIndex: e.detail.value
+    })
+  },
+  bindCountryChange: function (e) {
+    console.log('picker country 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryIndex: e.detail.value
+    })
+  },
+  bindAccountChange: function (e) {
+    console.log('picker account 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      accountIndex: e.detail.value
+    })
+  },
+  bindAgreeChange: function (e) {
+    this.setData({
+      isAgree: !!e.detail.value.length
+    });
+  },
+  toast1Change: function (e) {
+    this.setData({ toast1Hidden: true });
+  },
+  //弹出确认框  
+  modalTap: function (e) {
+    this.setData({
+      modalHidden: false
+    })
+  },
+  confirm_one: function (e) {
+    console.log(e);
+    this.setData({
+      modalHidden: true,
+      toast1Hidden: false,
+      notice_str: '提交成功'
+    });
+  },
+  cancel_one: function (e) {
+    console.log(e);
+    this.setData({
+      modalHidden: true,
+      toast1Hidden: false,
+      notice_str: '取消成功'
+    });
+  },
+  //弹出提示框  
+  modalTap2: function (e) {
+    this.setData({
+      modalHidden2: false
+    })
+  },
+  modalChange2: function (e) {
+    this.setData({
+      modalHidden2: true
+    })
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数  
+  },
+  onReady: function () {
+    // 页面渲染完成  
+  },
+  onShow: function () {
+    // 页面显示  
+  },
+  onHide: function () {
+    // 页面隐藏  
+  },
+  onUnload: function () {
+    // 页面关闭  
+  },
+  formSubmit: function (e) {
+    var that = this;
+    var formData = e.detail.value;
+    wx.request({
+      //url: 'http://test.com:8080/test/socket.php?msg=2',
+      data: formData,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.modalTap();
+      }
+    })
+  },
+  formReset: function () {
+    console.log('form发生了reset事件');
+    this.modalTap2();
+  }
+}) 
