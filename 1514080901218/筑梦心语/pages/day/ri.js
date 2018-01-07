d21ecd517176fab29bc40b3f17795cb7d519d6cf
lzhxh <821448002@qq.com>
2018-01-07 20:10:51 +0800

    #5 #101 第五次实验代码

---
+++ 1514080901218/筑梦心语/pages/day/ri.js
@@ -0,0 +1,26 @@
+// pages/ripiaofang/ri.js
+Page({
+
+  data: {
+    ri: [],
+    showapi_res_code:[]
+  },
+  onLoad: function () {
+    var that = this;
+
+    wx.request({
+      url: "https://route.showapi.com/578-2?showapi_appid=49955&showapi_sign=9911fe96a0e747b79ae822f20fedaede&",
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) {
+        var data = res.data;
+        console.log(data);
+        that.setData({
+          ri:data.showapi_res_body.datalist
+        })
+      },
+    })
+
+  }
+})
